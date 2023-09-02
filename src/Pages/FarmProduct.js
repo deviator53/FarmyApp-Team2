4782f514665908c7dc8635677857a42494ab2e43
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-09-02 23:50:41 +0100

    Feat:designed and built the product list

--- src/Pages/FarmProduct.js
+++ src/Pages/FarmProduct.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import FarmProductLists from '../Components/FarmProductLists';
 
 const FarmProduct = () => {
   return (
     <div>
       <Navbar />
+      <FarmProductLists />
       <Footer />
     </div>
   )
